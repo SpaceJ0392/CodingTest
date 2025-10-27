f7b8048a150f25929de22d0881f19b2109941a12
cosmosJ <jwj0392@gmail.com>
2025-10-27 14:35:16 +0900

    feat(programmers) : add CarryGoldAndSliver sol

--- Java/src/CarryGoldAndSliver.java
+++ Java/src/CarryGoldAndSliver.java
@@ -1,2 +1,42 @@
 public class CarryGoldAndSliver {
+
+    public static void main(String[] args) {
+        CarryGoldAndSliver obj = new CarryGoldAndSliver();
+        long ans = obj.solution(90, 500, new int[]{70, 70, 0}, new int[]{0, 0, 500}, new int[]{100, 100, 2}, new int[]{4, 8, 1});
+        System.out.println(ans);
+    }
+
+    public long solution(int a, int b, int[] g, int[] s, int[] w, int[] t) {
+        long answer = -1;
+        long start = 0, end = 400_000_000_000_000L;
+
+        while(start <= end){
+            long mid = (start + end) / 2;
+            if(isPossible(mid, a, b, g, s, w, t)) {
+                answer = mid;
+                end = mid - 1l;
+            }
+            else start = mid + 1l;
+        }
+
+        return answer;
+    }
+
+    public boolean isPossible(long now, int goalG, int goalS,
+                              int[] cityG, int[] cityS, int[] weight, int[] time){
+
+
+        long nowG = 0, nowS = 0, nowTot = 0;
+        for(int i = 0; i < weight.length; i++){
+            int round = time[i] * 2;
+            long cnt = now / round;
+            if((now % round) >= time[i]) cnt++;
+
+            long totW = weight[i] * cnt;
+            nowG += Math.min(totW, cityG[i]);
+            nowS += Math.min(totW, cityS[i]);
+            nowTot += Math.min(totW, cityG[i] + cityS[i]);
+        }
+        return goalG <= nowG && goalS <= nowS && goalG + goalS <= nowTot;
+    }
 }
--- Java/src/algorithm/FloydWarshall.java
+++ Java/src/algorithm/FloydWarshall.java
@@ -1,4 +1,36 @@
 package algorithm;
 
+import java.util.Arrays;
+
+/*
+    Dijkstra 와 FloydWarsShall의 차이점?
+    - Dijkstra : 한 노드에서의 다른 모든 간선에서의 최단 거리 확보.
+    - FloydWarsShall : 모든 노드에서 다른 모든 노드간의 최단 거리 확보.
+*/
+
 public class FloydWarshall {
+    private static final int INF = Integer.MAX_VALUE;
+
+
+    public static void main(String[] args) {
+        FloydWarshall fw = new FloydWarshall();
+        int[][] ans = fw.solution(new int[][]{{0, 5, INF, 10}, {INF, 0, 2, INF}, {INF, INF, 0, 1}, {INF, INF, INF, 0}});
+        System.out.println(Arrays.deepToString(ans));
+    }
+
+    public int[][] solution(int[][] graph) {
+        int len = graph.length;
+
+        for(int mid = 0; mid < len; mid++) {
+            for (int start = 0; start < len; start++) {
+                for (int end = 0; end < len; end++) {
+                    if (graph[start][mid] != INF && graph[mid][end] != INF) {
+                        graph[start][end] = Math.min(graph[start][end], graph[start][mid] + graph[mid][end]);
+                    }
+                }
+            }
+        }
+
+        return graph;
+    }
 }
